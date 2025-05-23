8579ad0c75a8121eeaad6abd40142591cfc6f8d3
Aditya Borkar <aditya.borkar.programs@gmail.com>
2025-05-23 17:16:43 +0000

    wip

--- src/api/actions/workspace/route.ts
+++ src/api/actions/workspace/route.ts
@@ -1,13 +1,13 @@
-import { workspace } from "drizzle/schema/workspace";
-import { eq } from "drizzle-orm";
-import db from "@/lib/server-db";
+import { workspace } from 'drizzle/schema/workspace';
+import { eq } from 'drizzle-orm';
+import db from '@/lib/server-db';
 
 export async function POST(req: Request) {
-	const user = { slug: "adityaborkar" };
+	const user = { slug: 'adityaborkar' };
 
 	const { name, tdd, ide: IDE } = await req.json();
-	const ide = IDE.sort((a: string, b: string) => a.localeCompare(b)).join(",");
-	const slug = `${user.slug}/${name.toLowerCase().trim().replace(/ /g, "-")}`;
+	const ide = IDE.sort((a: string, b: string) => a.localeCompare(b)).join(',');
+	const slug = `${user.slug}/${name.toLowerCase().trim().replace(/ /g, '-')}`;
 	const data = { name, tdd, ide, slug };
 	const ws = await db.insert(workspace).values(data);
 	return new Response(JSON.stringify({ success: true, data: ws }));
@@ -15,10 +15,10 @@ export async function POST(req: Request) {
 
 export async function GET(req: Request) {
 	const { searchParams } = new URL(req.url);
-	const slug = searchParams.get("slug");
+	const slug = searchParams.get('slug');
 	if (!slug)
 		return new Response(
-			JSON.stringify({ success: false, error: "Slug is required" }),
+			JSON.stringify({ success: false, error: 'Slug is required' }),
 		);
 
 	const [ws] = await db
@@ -27,7 +27,7 @@ export async function GET(req: Request) {
 		.where(eq(workspace.slug, slug));
 	if (!ws)
 		return new Response(
-			JSON.stringify({ success: false, error: "Workspace not found" }),
+			JSON.stringify({ success: false, error: 'Workspace not found' }),
 		);
 
 	return new Response(JSON.stringify({ success: true, data: ws }));
